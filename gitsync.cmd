64c3fae47493b7e5263b5f68921098cb531d7fe1
formr <formr@klicat.com>
2019-07-29 13:13:05 +0200

    Commit by formr

---
+++ gitsync.cmd
@@ -0,0 +1,4 @@
+git -c user.email=formr@klicat.com -c user.name=formr pull origin master
+git -c user.email=formr@klicat.com -c user.name=formr add .
+git -c user.email=formr@klicat.com -c user.name=formr commit -a -m "Commit by formr"
+git -c user.email=formr@klicat.com -c user.name=formr push
